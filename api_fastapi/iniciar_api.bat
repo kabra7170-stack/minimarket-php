f091dc8f00a28ae6de27c95252441c6c605ce7e4
kabra7170-stack <kabra7170@gmail.com>
2026-05-10 11:32:07 -0400

    Eliminar api_fastapi

--- api_fastapi/iniciar_api.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Iniciando MiniMarket G2 API...
-cd /d C:\Users\Deurys\Downloads\xampp\htdocs\minimarket\api_fastapi
-py -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
-pause
